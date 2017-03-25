d3d8ba39a9c14b4bb5efbe5bae7e8a3d350583be
Joald <joald@joald-linux1.(none)>
2017-03-25 20:24:24 +0100

    bash+make

--- test.sh
+++ test.sh
@@ -1,1 +1,11 @@
-for f in tests/*.in; do ./test <$f | diff - "${f%.*}.out";done
+if [ $1 = "-v" ] ; then
+	for f in $3/*.in; do 
+		./$2 -v <$f 2> "diag.err" | diff -q - "${f%.*}.out";
+		diff -q "diag.err" "${f%.*}.err";
+	done
+	rm "diag.err"
+else
+	for f in $2/*.in; do 
+		./$1 <$f | diff -q - "${f%.*}.out";
+	done
+fi
